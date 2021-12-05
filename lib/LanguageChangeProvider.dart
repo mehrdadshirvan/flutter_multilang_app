b4ca5c61921596e58efd9a63a8cb498a81d78d60
mehrdadshirvan <34993335+mehrdadshirvan@users.noreply.github.com>
2021-12-05 16:15:24 +0330

    debugging

--- lib/LanguageChangeProvider.dart
+++ lib/LanguageChangeProvider.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 
 class LanguageChangeProvider with ChangeNotifier {
-  Locale _currentLocale = Locale('en');
+  Locale _currentLocale = Locale('fa');
 
   Locale get currentLocaleLang => _currentLocale;
 
